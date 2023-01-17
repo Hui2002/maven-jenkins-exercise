32a265576342db46bff512eaf236e3ac9282c3d0
chewzhenghui <zhheng.hui@gmail.com>
2023-01-17 15:48:57 +0800

    commit

---
+++ src/main/java/com/devops/jenkins_maven/eclipse/App.java
@@ -0,0 +1,13 @@
+package com.devops.jenkins_maven.eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
